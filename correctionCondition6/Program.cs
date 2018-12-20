0d434abecd1c68c7322f8d6ead3b06cac30df018
maheir <maheirne@gmail.com>
2018-12-20 13:57:57 +0100

    correction

---
+++ correctionCondition6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace correctionCondition6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choisissez un mois ");
+            string months = Console.ReadLine();
+            switch (months)
+            {
+                case "Mars":
+                case "Avril":
+                case "Mai":
+                    Console.WriteLine("C'est le printemps");
+                    break;
+                case "Juin":
+                case "Juillet":
+                case "Aout":
+                    Console.WriteLine("C'est l'été");
+                    break;
+                case "Septembre":
+                case "Octobre":
+                case "Novembre":
+                    Console.WriteLine("C'est l'automne");
+                    break;
+                case "Décembre":
+                case "Janvier":
+                case "Février":
+                    Console.WriteLine("C'est l'hiver");
+                    break;
+                default:
+                    Console.WriteLine("Ceci n'est pas un mois");
+                    break;
+            }
+        }
+    }
+}
